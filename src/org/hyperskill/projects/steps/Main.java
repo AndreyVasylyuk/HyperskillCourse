a933d03e3e3a4f88b191dae6b5e07da51eb5939e
AndreyVasylyuk <AndreyVasylyuk@gmail.com>
2021-10-29 23:11:39 +0300

    added Main.java

---
+++ src/org/hyperskill/projects/steps/Main.java
@@ -0,0 +1,87 @@
+package org.hyperskill.projects.steps;
+
+import java.util.Scanner;
+
+class Main {
+	public static void main(String[] args) {
+		/*
+		Scanner scanner = new Scanner(System.in);
+		int sum = 0;
+
+		while (scanner.hasNext()) {
+			int current = scanner.nextInt();
+            if (current == 0) break;
+			if (current % 10 == 0) {
+				sum += current;
+			}
+		}
+		System.out.println(sum);
+		
+		
+		for (int i = 0; i < 5; i++) { 
+		    for (char letter = 'a'; letter <= 'f'; letter++) { 
+		        if (letter < 'f') { 
+		            continue; 
+		        } 
+		        System.out.println(letter); 
+		    } 
+		}
+		*/
+//		int a = 2;
+//		switch (a) {
+//			case 2: 
+//				System.out.println(1);
+//				break;
+//			case 3: 
+//				System.out.println(1);
+//				break;
+//		}
+		
+//		Scanner scanner = new Scanner(System.in);
+//        long m = scanner.nextLong();
+//        int n = 1;
+//        long smallest = 1;
+//        while (smallest <= m) {
+//            smallest *= ++n;
+//        }
+//        System.out.println(n-1);
+		
+		/*
+		Scanner scanner = new Scanner(System.in);
+        int elementsNumber = scanner.nextInt();
+        
+        for (int i = 1; i <= Integer.MAX_VALUE; i++) {
+            for (int j = 0; j < i; j++) {
+                if (elementsNumber < 1) break;
+                System.out.print(i + " ");
+                elementsNumber--;
+            }
+        }
+        */
+
+//		long M = 1;
+//		long P = 10;
+//		long K = 10;
+//		int n = 0;
+//		
+//
+//		M = M + M * P / 100;
+//		n++;
+//		System.out.print(n);
+//		System.out.println(M);
+//		while (M <= K) {
+//			if (K == M) {
+//				System.out.println(0);
+//			} else {
+//				M = M + M * P / 100;
+//				n++;
+//				System.out.print(n);
+//				System.out.println(M);
+	}
+        
+        
+        
+       
+    
+
+}
